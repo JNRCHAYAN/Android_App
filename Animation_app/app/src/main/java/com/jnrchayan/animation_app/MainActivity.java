df695c3d76679698a3b03e08f09c84f5aeee1570
Jogendra nath roy chayan <jogendranathroy40@gmail.com>
2023-07-02 00:49:20 +0600

    Update MainActivity.java

--- Animation_app/app/src/main/java/com/jnrchayan/animation_app/MainActivity.java
+++ Animation_app/app/src/main/java/com/jnrchayan/animation_app/MainActivity.java
@@ -9,28 +9,43 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
+LottieAnimationView lc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView texxt;
-        Button bt,bta,btr,sc;
+        Button bt,bta,btr,sc,cool;
         bt = findViewById(R.id.btt);
         bta = findViewById(R.id.bta);
         btr = findViewById(R.id.btr);
         texxt = findViewById(R.id.tex);
         sc = findViewById(R.id.bts);
+        cool =findViewById(R.id.cool);
+
         Animation move = AnimationUtils.loadAnimation(this,R.anim.move);
         Animation alph = AnimationUtils.loadAnimation(this,R.anim.alpha);
         Animation rote = AnimationUtils.loadAnimation(this,R.anim.rote);
         Animation scal = AnimationUtils.loadAnimation(this,R.anim.scal);
+
+        cool.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         sc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 texxt.startAnimation(scal);
             }
+
+
         });
         btr.setOnClickListener(new View.OnClickListener() {
             @Override
